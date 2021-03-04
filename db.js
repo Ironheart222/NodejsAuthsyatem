8827169e5dd4eb8ee6fba9829c7b2db58f8991ef
Ironheart222 <kanopatel222.kp@gmail.com>
2021-03-04 18:57:26 +0530

    init

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://username:password@cluster0.nldzu.mongodb.net/mydb?retryWrites=true&w=majority', { useMongoClient: true });
